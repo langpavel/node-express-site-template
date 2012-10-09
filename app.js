9483ecfab27ba55748f6afbf9e48f1b79b98df6b
Pavel Lang <langpavel@phpskelet.org>
2012-10-09 21:30:48 +0200

    redis session store

--- app.js
+++ app.js
@@ -5,29 +5,56 @@
 
 var express = require('express');
 var path = require('path');
+
+var config = require('./config');
+
 var routes = require('./routes');
 var user = require('./routes/user');
+
 var app = module.exports = express();
 
 
-app.configure(function() {
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', path.join(__dirname, 'views'));
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser('ogXMXgRbnInguKYYx9Pm'));
-  app.use(express.session());
-  app.use(app.router);
-  app.use(require('stylus').middleware(path.join(__dirname, 'public')));
-  app.use(express.static(path.join(__dirname, 'public')));
-});
+app.Redis = require('redis');
+app.redisCreateClient = function() {
+  return app.Redis.createClient(config.redis.port,
+                                config.redis.host,
+                                config.redis.options);
+};
+app.redis = app.redisCreateClient();
 
-app.configure('development', function() {
-  app.use(express.errorHandler());
-});
+
+
+app.set('port', process.env.PORT || config.http.port || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(express.favicon());
+
+app.use(express.logger('dev'));
+
+app.use(express.bodyParser());
+
+app.use(express.methodOverride());
+
+// sessions
+app.RedisSessionStore = require('connect-redis')(express);
+config.redisSessionStore.client = app.redisCreateClient();
+app.sessionStore = new app.RedisSessionStore(config.redisSessionStore);
+
+app.use(express.cookieParser('ogXMXgRbnInguKYYx9Pm'));
+config.session.store = app.sessionStore;
+app.use(express.session(config.session));
+
+if (config.csrf.enabled) {
+  app.use(express.csrf(config.csrf));
+}
+
+app.use(app.router);
+
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.errorHandler());
 
 app.get('/', routes.index);
 app.get('/users', user.list);
--- config.js
+++ config.js
@@ -1,6 +1,41 @@
 module.exports = {
 
+  http: {
+    port: 3000
+  },
+
   redis: {
+    host: 'localhost',
+    options: {}
+  },
+
+  redisSessionStore: {
+    ttl: 60 * 60 * 24 * 30, // = 30 days (in seconds)
+    db: 0,
+    prefix: 'sess:'
+  },
+
+  session: {
+    /*
+    key       cookie name defaulting to connect.sid
+    store     session store instance
+    secret    session cookie is signed with this secret to prevent tampering
+    cookie    session cookie settings, defaulting to
+              { path: '/', httpOnly: true, maxAge: null }
+    proxy     trust the reverse proxy when setting secure cookies
+              (via "x-forwarded-proto")
+    */
+
+    key: 'sid',
+    cookie: {
+      path: '/',
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 30 // = 30 days (in miliseconds)
+    }
+  },
+
+  csrf: {
+    enabled: true
   }
 
 };
