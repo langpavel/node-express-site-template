11663d7e72eb67c819f509e55069ed18399dc6a6
Pavel Lang <langpavel@phpskelet.org>
2012-10-20 14:09:50 +0200

    Mongo and redis developmnet preconfiguration

--- app/index.js
+++ app/index.js
@@ -15,6 +15,11 @@ var app = module.exports = express();
 
 app.Redis = require('redis');
 app.redisCreateClient = function() {
+  if (config.redis.socket) {
+    config.redis.port = config.redis.socket;
+    config.redis.host = null;
+  }
+
   return app.Redis.createClient(config.redis.port,
                                 config.redis.host,
                                 config.redis.options);
